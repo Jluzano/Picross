93f008d315dd14559bb1b0b019a00a5f028e0e18
Justin <justinluzano@gmail.com>
2024-06-25 18:34:28 -0700

    Code overhaul
    Edited code on another machine and posting new code to repository

--- Assets/Scripts/CellScript.cs
+++ Assets/Scripts/CellScript.cs
@@ -1,29 +1,86 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using System.Runtime.CompilerServices;
-using UnityEditor.Experimental.GraphView;
+using System.Collections.Generic;
 
-public class Cell : MonoBehaviour, IPointerClickHandler
+public class Cell : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     public Image tile;
     public Sprite regularTile;
     public Sprite grayXTile;
     public Sprite blueTile;
-    
-    //private bool locked = false;
+
     public int state = 0;
-    //State 0 = Blank
-    //State 1 = Marked
-    //State 2 = X
+    // State 0 = Blank
+    // State 1 = Marked
+    // State 2 = X
+
+    private bool isLeftPointerDown = false;
+    private bool isRightPointerDown = false;
+    private static HashSet<Cell> processedCells = new HashSet<Cell>();
+
+    private Nonogram nonogramScript;
 
     private void Start()
     {
         tile = GetComponent<Image>();
+        nonogramScript = FindObjectOfType<Nonogram>();
     }
-    public void OnPointerClick(PointerEventData eventData)
+
+    public void OnPointerDown(PointerEventData eventData)
     {
         if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            isLeftPointerDown = true;
+            processedCells.Clear();
+            ChangeTileState(true);
+            processedCells.Add(this);
+        }
+        else if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            isRightPointerDown = true;
+            processedCells.Clear();
+            ChangeTileState(false);
+            processedCells.Add(this);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            isLeftPointerDown = false;
+        }
+        else if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            isRightPointerDown = false;
+        }
+        processedCells.Clear();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if ((isLeftPointerDown || isRightPointerDown) && eventData.pointerEnter != null)
+        {
+            Cell cell = eventData.pointerEnter.GetComponent<Cell>();
+            if (cell != null && !processedCells.Contains(cell))
+            {
+                if (isLeftPointerDown)
+                {
+                    cell.ChangeTileState(true);
+                }
+                else if (isRightPointerDown)
+                {
+                    cell.ChangeTileState(false);
+                }
+                processedCells.Add(cell);
+            }
+        }
+    }
+
+    private void ChangeTileState(bool isLeftClick)
+    {
+        if (isLeftClick)
         {
             if (this.state == 0)
             {
@@ -36,18 +93,23 @@ public class Cell : MonoBehaviour, IPointerClickHandler
                 this.state = 0;
             }
         }
-        else if (eventData.button == PointerEventData.InputButton.Right)
+        else
         {
-            if(this.state == 0)
+            if (this.state == 0)
             {
                 tile.sprite = grayXTile;
                 this.state = 2;
             }
-            else if(this.state == 2)
+            else if (this.state == 2)
             {
                 tile.sprite = regularTile;
                 this.state = 0;
             }
+        }
+
+        if (nonogramScript != null)
+        {
+            nonogramScript.CheckSolution();
         }
     }
 }
--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -1,27 +1,130 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameScript : MonoBehaviour
+public class Nonogram : MonoBehaviour
 {
-    public GameObject cell;
+    public GameObject cellPrefab;
     public GameObject grid;
-    
-    int[] solution;
-    // Start is called before the first frame update
+
+    private int gridSize = 5; //size of the grid
+    private float cellSpacing = 1.1f; //cell spacing
+
+    private int[,] gridArray; // 0 = unknown, 1 = filled, -1 = empty
+
+    // Clues for rows and columns
+    private List<int>[] rowClues = new List<int>[5];
+    private List<int>[] columnClues = new List<int>[5];
+
+    // Solution grid, replace with JSON
+    private int[,] solutionArray = new int[,]
+    {
+        { 1, 1, 0, 0, 0 },
+        { 1, 0, 0, 1, 0 },
+        { 1, 1, 1, 0, 0 },
+        { 1, 0, 0, 1, 0 },
+        { 1, 1, 0, 0, 0 }
+    };
+
     void Start()
     {
-        for(int i = 0; i < 25; i++)
+        gridArray = new int[gridSize, gridSize];
+
+        // Initialize the grid visually
+        InitializeGrid();
+        //Solve the grid
+        Solve();
+    }
+
+    void InitializeGrid()
+    {
+        //createing a 2d array
+        for (int row = 0; row < gridSize; row++)
         {
-            var test = Instantiate(cell);
-            test.transform.SetParent(grid.transform);
-            test.transform.localScale = new Vector3(1, 1, 1);
+            for (int col = 0; col < gridSize; col++)
+            {
+                var newCell = Instantiate(cellPrefab); //creating a cell
+                newCell.transform.SetParent(grid.transform); //setting parent to grid
+                newCell.transform.localScale = new Vector3(1, 1, 1); // adjusting size
+
+                //spacing between cells
+                float xPos = col * cellSpacing;
+                float yPos = row * cellSpacing;
+                //moving cell into position
+                newCell.transform.localPosition = new Vector3(xPos, yPos, 0);
+
+                // Initialize grid array to unknown (0)
+                gridArray[row, col] = 0;
+            }
+        }
+    }
+    void Solve()
+    {
+        //looping through 2d array named gridArray
+        for (int row = 0; row < gridSize; row++)
+        {
+            for (int col = 0; col < gridSize; col++)
+            {
+                var cell = grid.transform.GetChild(row * gridSize + col).GetComponent<Cell>();
+                
+                // Set the cell state based on the solution array
+                if (cell.state != solutionArray[row, col])
+                {
+                    //If the tile state is 0 but is a 1 in the solution array, change state to 1 and color to blue
+                    cell.tile.sprite = cell.blueTile;
+                    cell.state = 1;
+                }
+            }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateGridArray()
     {
-        //int test2 = cell.state;
+        //Updating the 2d array to be compared with the solution
+        for (int row = 0; row < gridSize; row++)
+        {
+            for (int col = 0; col < gridSize; col++)
+            {
+                //grabbing the cell
+                var cell = grid.transform.GetChild(row * gridSize + col).GetComponent<Cell>();
+                gridArray[row, col] = cell.state;
+            }
+        }
+    }
+
+    public void CheckSolution()
+    {
+        UpdateGridArray();
+        bool isCorrect = true;
+
+        for (int row = 0; row < gridSize; row++)
+        {
+            for (int col = 0; col < gridSize; col++)
+            {
+                // Ignore cells with state 2 (X)
+                if (gridArray[row, col] == 2)
+                {
+                    continue;
+                }
+
+                if (gridArray[row, col] != solutionArray[row, col])
+                {
+                    isCorrect = false;
+                    break;
+                }
+            }
+            if (!isCorrect) break;
+        }
+
+        if (isCorrect)
+        {
+            Debug.Log("Finished!");
+        }
+        else
+        {
+            Debug.Log("Still incomplete.");
+        }
     }
 }
